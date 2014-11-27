33223fd2e941968865568e3f18928bdb56bbec75
mtrop <mtropiano@gmail.com>
2014-11-27 04:34:20 -0500

    Changed how sound groups work.

--- src/com/blackrook/oal/util/stage/OALSoundGroup.java
+++ src/com/blackrook/oal/util/stage/OALSoundGroup.java
@@ -10,30 +10,105 @@ package com.blackrook.oal.util.stage;
 /**
  * A sound group for OALSoundStage2D.
  * Sounds played within this group apply this group's gain and pitch characteristics
- * combined with the object's characteristics.
+ * combined with the sound's characteristics.
+ * <p>
+ * These can also set a parent group that influences the gain and pitch of lower groups.
  * @author Matthew Tropiano
  */
-public interface OALSoundGroup
+public class OALSoundGroup
 {
+	/** Parent group. */
+	private OALSoundGroup parent;
+	
+	/** Scalar bias for gain level. */
+	private float gainBias;
+	/** Scalar bias for pitch level. */
+	private float pitchBias;
+	/** All sounds played to this observer are relative to the listener, if set. */
+	private boolean relative;
+	/** Maximum amount of voices to use in this group. */
+	private int maxVoices;
+
+	public OALSoundGroup()
+	{
+		parent = null;
+		gainBias = 1f;
+		pitchBias = 1f;
+		maxVoices = 0;
+		relative = true;
+	}
+	
 	/**
-	 * Gets the gain bias for this group.
+	 * Sets this group's parent, if any.
 	 */
-	public float getGainBias();
+	public void setParent(OALSoundGroup parent)
+	{
+		this.parent = parent;
+	}
+	
+	/**
+	 * Gets the gain bias for this group (affected by parents, if any).
+	 */
+	public float getGainBias()
+	{
+		return (parent != null ? parent.getGainBias() : 1f) * gainBias;
+	}
 
 	/**
-	 * Gets the pitch bias for this group.
+	 * Sets the gain bias for this group.
 	 */
-	public float getPitchBias();
+	public void setGainBias(float gainBias)
+	{
+		this.gainBias = gainBias;
+	}
+
+	/**
+	 * Gets the pitch bias for this group (affected by parents, if any).
+	 */
+	public float getPitchBias()
+	{
+		return (parent != null ? parent.getPitchBias() : 1f) * pitchBias;
+	}
+
+	/**
+	 * Sets the pitch bias for this group.
+	 */
+	public void setPitchBias(float pitchBias)
+	{
+		this.pitchBias = pitchBias;
+	}
 
 	/** 
 	 * Gets if all sounds played to this group are relative to the listener object. 
 	 */
-	public boolean isRelative();
+	public boolean isRelative()
+	{
+		return relative;
+	}
+
+	/** 
+	 * Sets if all sounds played in this group are relative to the listener object. 
+	 */
+	public void setRelative(boolean relative)
+	{
+		this.relative = relative;
+	}
 
 	/**
 	 * Gets the maximum amount of voices that can be played concurrently in this group.
 	 * If this returns 0, there is no max (except for the global maximum). 
 	 */
-	public int getMaxVoices();
+	public int getMaxVoices()
+	{
+		return maxVoices;
+	}
+
+	/**
+	 * Sets the maximum amount of voices that can be played concurrently in this group. 
+	 */
+	public void setMaxVoices(int maxVoices)
+	{
+		this.maxVoices = maxVoices;
+	}
 
 }
